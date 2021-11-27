afa6f994b713071901813bed17c21cf69b3b37ad
Mark Tutkowski <mark@tutkowski.com>
2021-11-27 15:24:02 -0600

    Add start script

---
+++ start.sh
@@ -0,0 +1,16 @@
+## In order to start the server when the pi boots
+## edit /etc/rc.local and have it call this file.
+## i.e. "./start.sh &"
+
+# Clean repo
+git clean -xfd;
+git pull;
+
+# Rebuild frontend
+cd ./frontend;
+npm install;
+npm run build -- --prod
+
+# Start server
+cd ../backend;
+python3 server.python3

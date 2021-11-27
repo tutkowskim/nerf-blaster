0fd09a6d789a27d1f80bb90a2078db07847d5878
Mark Tutkowski <mark@tutkowski.com>
2021-11-27 15:41:49 -0600

    Just start the server

--- start.sh
+++ start.sh
@@ -2,15 +2,5 @@
 ## edit /etc/rc.local and have it call this file.
 ## i.e. "./start.sh &"
 
-# Clean repo
-git clean -xfd;
-git pull;
-
-# Rebuild frontend
-cd ./frontend;
-npm install;
-npm run build -- --prod
-
-# Start server
 cd ../backend;
-python3 server.py
+python3 server.py;
